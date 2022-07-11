9b98745c36739b15b56cadf4d7c6e98f83fdf4e3
Camille Bergeron <camille.bergeron@boston.gov>
2022-07-11 12:32:51 -0400

    Create starter_script.R

---
+++ starter_script.R
@@ -0,0 +1,8 @@
+# this wil be a strter script / test tutorial for arcgis
+
+###### SET UP ##########
+
+# WEBSITE: https://github.com/R-ArcGIS/r-bridge
+# install.packages("arcgisbinding", repos="https://r.esri.com", type="win.binary")
+library(arcgisbinding)
+arc.check_product()
